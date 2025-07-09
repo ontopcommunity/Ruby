6197e257a4c00a985109e9efdd5d03f39e46b822
nhatminhsieuvip <minh2109vn@gmail.com>
2025-07-09 19:47:32 +0700

    Create port.py

---
+++ port.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Bot is alive!'
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+Thread(target=run).start()
+import threading, time, requests
+
+def keep_alive():
+    while True:
+        try:
+            requests.get("https://ruby-chan.onrender.com/")
+        except:
+            pass
+        time.sleep(300)
+
+threading.Thread(target=keep_alive).start()
